cdada63da76eea43187d722c2fd204a53dca8159
Hermen <hermenluchtenbelt@hotmail.com>
2026-05-13 15:46:08 +0200

    Make icon path relative in make_icon.py

--- make_icon.py
+++ make_icon.py
@@ -1,13 +1,14 @@
+import os
 from PIL import Image, ImageDraw
 
 size = 256
 img = Image.new('RGBA', (size, size), (20, 20, 20, 255))
 draw = ImageDraw.Draw(img)
 
-# Draw a white checkmark on dark background
-# This won't turn blue when Windows applies overlay
+# Green checkmark on dark background
 draw.line([(60, 140), (100, 180)], fill=(34, 197, 94), width=24)
 draw.line([(100, 180), (200, 60)], fill=(34, 197, 94), width=24)
 
-img.save('C:\\Users\\lucht004\\Documents\\2do-app-desktop\\public\\icon.ico')
-print("Icon created")
+icon_path = os.path.join(os.path.dirname(__file__), 'public', 'icon.ico')
+img.save(icon_path)
+print(f"Icon created: {icon_path}")
